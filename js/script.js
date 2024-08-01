16035bf586ff849d83824a43b25f641e7b783364
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-01 10:34:57 +0200

    hw2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+//Task #1
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+//Task #2
+let planetName;
+let userName;
+
+//Task #3
+let result;
+result = '10' + 10;
+
+let typeofResult = typeof result;
+console.log(typeofResult);

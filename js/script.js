6c3ad5cb769d0af4286e7bb789a635d1e88f3967
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-02 09:24:15 +0200

    hw_3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const operandOne = prompt('Ввдіть перше число')
+const operandTwo = prompt('Ввдіть друге число')
+// Оскільки prompt() повертає уведені дані рядком потрібно перетворити на числа
+const numberOne = Number(operandOne)
+const numberTwo = Number(operandTwo)
+
+//Додавання
+console.log(`${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`)
+//Віднімання
+console.log(`${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`)
+//Множення
+console.log(`${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`)
+//Ділення
+console.log(`${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`)

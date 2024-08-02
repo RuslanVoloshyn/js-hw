3fc91065f544c5b4acb93383f63f78e36a1a7836
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-02 11:39:17 +0200

    Update hw_3

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const operandOne = prompt('Ввдіть перше число')
-const operandTwo = prompt('Ввдіть друге число')
+const operandOne = prompt('Ввдіть перше число');
+const operandTwo = prompt('Ввдіть друге число');
 // Оскільки prompt() повертає уведені дані рядком потрібно перетворити на числа
-const numberOne = Number(operandOne)
-const numberTwo = Number(operandTwo)
+const numberOne = Number(operandOne);
+const numberTwo = Number(operandTwo);
 
-//Додавання
-console.log(`${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`)
-//Віднімання
-console.log(`${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`)
-//Множення
-console.log(`${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`)
-//Ділення
-console.log(`${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`)
+//Додавання:
+console.log(`${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`);
+//Віднімання:
+console.log(`${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`);
+//Множення:
+console.log(`${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`);
+//Ділення:
+console.log(`${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`);

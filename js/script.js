986742c0f6c229a0436db36d262ebef5f422a0c8
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-04 18:43:43 +0200

    hw_4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+//Отримання та виведення даних:
+const name = prompt('What is your name?');
+const age = prompt('What is your age?');
+const likesProgramming = confirm('Do you like programming?');
+//Обробка та виведення даних
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(likesProgramming, typeof likesProgramming);
+
+//Робота з рядками
+const greeting = 'Ruslan';
+alert(`Привіт ${greeting}`);
+
+//Перетворення типів:
+const numericAge = Number(age);
+const nextAge = numericAge;
+console.log(`${nextAge} + 1`);
+//Порівняння значень:
+let isAdult = age >= 18;
+console.log(isAdult);

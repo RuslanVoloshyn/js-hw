17048b9dae08e414ca5f828824566e15f4c46839
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-07 18:10:05 +0200

    hw_5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const hours = prompt('Enter the number of hours');
+console.log(hours);
+
+
+if (isNaN(hours)) {
+	alert('Please enter a valid number');
+} else {
+	let numberHours = Number(hours);
+
+	if (numberHours < 0) {
+		alert(`you entered a negative number ${numberHours}`);
+	} else {
+		alert(`you entered a positive number ${numberHours}`);
+	}
+}
+
+let secondInAMinutes = 60;
+console.log(`${secondInAMinutes} second in minutes`);
+
+let minutesInAnHour = 60;
+console.log(`${minutesInAnHour} minutes in hours`);
+
+let secondsInAnHour = secondInAMinutes * minutesInAnHour;
+console.log(`${secondsInAnHour} seconds in one hour`);
+
+let result = hours * secondsInAnHour;
+alert(`${result} seconds in that number of hours`);

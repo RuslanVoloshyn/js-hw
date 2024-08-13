e78bc1bf108a647e6b38397b97df3f601ab29a76
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-13 11:40:15 +0200

    We ask the user

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,64 @@
 "use strict";
 
-let numOrStr = prompt("input number or string");
-console.log(numOrStr);
+// Запитуємо у користувача рік народження
+const birthYear = prompt("Введіть ваш рік народження:");
+if (!birthYear) {
+	alert("Шкода, що Ви не захотіли ввести свою дату народження.");
+} else {
+	// Запитуємо у користувача місто проживання
+	const city = prompt("В якому місті ви живете?");
+	if (!city) {
+		alert("Шкода, що Ви не захотіли ввести своє місто.");
+	} else {
+		// Запитуємо у користувача улюблений вид спорту
+		const sport = prompt("Який ваш улюблений вид спорту?");
+		if (!sport) {
+			alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+		} else {
+			// Розрахунок віку користувача
+			const currentYear = new Date().getFullYear();
+			const age = currentYear - birthYear;
 
-// if (numOrStr === null) {
-// 	console.log("ви скасували");
-// } else if (numOrStr.trim() === "") {
-// 	console.log("Empty String");
-// } else if (isNaN(+numOrStr)) {
-// 	console.log(" number is Ba_NaN");
-// } else {
-// 	console.log("OK!");
-// }
+			// Визначення повідомлення про місто
+			let cityMessage;
+			switch (city.toLowerCase()) {
+				case "київ":
+					cityMessage = "Ти живеш у столиці України.";
+					break;
+				case "вашингтон":
+					cityMessage = "Ти живеш у столиці США.";
+					break;
+				case "лондон":
+					cityMessage = "Ти живеш у столиці Великобританії.";
+					break;
+				case "варшава":
+					cityMessage = "Ти живеш у столиці Польщі.";
+					break;
+				default:
+					cityMessage = `Ти живеш у місті ${city}.`;
+			}
 
-switch (true) {
-	case numOrStr === null:
-		console.log("ви скасували");
-		break;
-	case numOrStr.trim() === "":
-		console.log("Empty String");
-		break;
-	case isNaN(+numOrStr):
-		console.log(" number is Ba_NaN");
-		break;
-	default:
-		console.log("OK!");
+			// Визначення повідомлення про спорт
+			let sportMessage;
+			switch (sport.toLowerCase()) {
+				case "бокс":
+					sportMessage = "Круто! Хочеш стати, як Олександр Усик?";
+					break;
+				case "футбол":
+					sportMessage = "Круто! Хочеш стати, як Ліонель Мессі?";
+					break;
+				case "баскетбол":
+					sportMessage = "Круто! Хочеш стати, як Майкл Джордан?";
+					break;
+				case "теніс":
+					sportMessage = "Круто! Хочеш стати, як Роджер Федерер?";
+					break;
+				default:
+					sportMessage = `Цікавий вибір спорту: ${sport}.`;
+			}
+
+			// Показуємо результат
+			alert(`Вам ${age} років.\n${cityMessage}\n${sportMessage}`);
+		}
+	}
 }

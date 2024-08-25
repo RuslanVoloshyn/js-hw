fe5a3d2dee30874041ebfdbf0c5b804a56bff47d
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-25 16:01:33 +0200

    Execution Context

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-for (let i = 10; i <= 20; i++) {
-    console.log(`${+i} , `);
+function variableScopeDemo() {
+    for (var i = 0; i < 3; i++) {
+        var varVariable = 'Я оголошений через var';
+        let letVariable = 'Я оголошений через let';
+        const constVariable = 'Я оголошений через const';
+
+        console.log(`Всередині циклу (ітерація ${i}):`);
+        console.log(`var: ${varVariable}`);
+        console.log(`let: ${letVariable}`);
+        console.log(`const: ${constVariable}`);
+    }
+    console.log(`Поза циклом:`);
+    console.log(`var поза циклом: ${varVariable}`);
 }
+variableScopeDemo();

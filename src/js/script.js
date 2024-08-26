d342c8162d803ce67f2a09046da5be6409da695c
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-08-26 12:33:03 +0200

    randomIndex

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let generateKey = function (length, characters) {
+    let key = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        key += characters[randomIndex];
+    }
+    return key;
+};
+
+const characters = 'SHHSSDhwhwhrwhre35343467';
+const key = generateKey(21, characters);
+console.log(key);

f006366b3db480c17be514813d3582c34b79db4c
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-01 18:37:17 +0200

    remove the item element from the array array

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+let removeElement = function (array, item) {
+    const index = array.indexOf(item);
+
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+};
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);

d3572c2c731c70770e2e169fd6148b73ad8688ef
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-06 15:44:28 +0200

    Callbacks

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+function exponentiation(base, power) {
+    const result = base ** power;
+    alert(`Результат: ${base}**${power} = ${result}`);
+}
+
+function mainFunction(callback) {
+    const base = parseFloat(prompt('Введіть число (база):'));
+    const power = parseFloat(prompt('Введіть степінь:'));
+    callback(base, power);
+}
+
+mainFunction(exponentiation);
+
+function multiplication(base, power) {
+    const result = base * power;
+    alert(`Результат: ${base}*${power} = ${result}`);
+}
+
+function mainFunction1(callback) {
+    const base = parseFloat(prompt('Введіть число (база):'));
+    const power = parseFloat(prompt('Введіть число на яке бажаєте поділити:'));
+    callback(base, power);
+}
+
+mainFunction2(multiplication);
+
+function division(base, power) {
+    const result = base / power;
+    alert(`Результат: ${base}/${power} = ${result}`);
+}
+
+function mainFunction2(callback) {
+    const base = parseFloat(prompt('Введіть число (база):'));
+    const power = parseFloat(prompt('Введіть число на яке бажаєте поможити:'));
+    callback(base, power);
+}
+
+mainFunction1(division);
+
+function modulo(base, power) {
+    const result = base % power;
+    alert(`Результат: ${base}%${power} = ${result}`);
+}
+
+function mainFunction3(callback) {
+    const base = parseFloat(prompt('Введіть число (база):'));
+    const power = parseFloat(
+        prompt('Введіть число на від якого бажаєте побачити залишок : ')
+    );
+    callback(base, power);
+}
+
+mainFunction3(modulo);

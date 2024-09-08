c2d2177954f656bfc594676e21b6b25e41323a54
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-08 17:56:15 +0200

    Array sort, push, splice

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let lengthArray = +prompt('Введіть довжину масиву');
+
+let usersArray = [];
+
+for (let i = 0; i < lengthArray; i++) {
+    let item = prompt(`Введіть елемент для індексу ${i}:`);
+    usersArray.push(item);
+}
+
+console.log(`Масив після уведення даних користувачем ${usersArray}`);
+
+usersArray.sort((a, b) => {
+    return a - b;
+});
+
+console.log(`Масиви після сортоввання: ${usersArray}`);
+
+usersArray.splice(2, 3);
+console.log(`Масив після видалення з 2 по 4 елементи: ${usersArray}`);

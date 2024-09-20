2b31cd11f24b591a1ec6f86f286f3774d59fd1d1
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-20 16:59:55 +0200

    bind

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function InsertWordInto(str) {
+    let lastIndex = 0;
+    const words = str.split(' ');
+
+    return function (wordToInsert) {
+        if (lastIndex === 0) {
+            words.unshift(wordToInsert);
+        } else if (lastIndex === words.length) {
+            words.push(wordToInsert);
+        } else {
+            words.splice(lastIndex, 0, wordToInsert);
+        }
+        lastIndex++;
+
+        if (lastIndex > words.length) lastIndex = 0;
+
+        return words.join(' ');
+    };
+}
+
+const insert = InsertWordInto('hello world');
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));

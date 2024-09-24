5d0c8dc97499afe54d683d8083254f697fce586c
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-24 12:25:18 +0200

    this chain

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let ladder = {
+    step: 3,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    },
+};
+ladder.up().up().down().showStep();

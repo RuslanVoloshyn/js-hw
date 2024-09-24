e4343afa4aa21d643867787da03237f9666902de
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-24 11:22:05 +0200

    Locking function

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const calcFunction = () => {
+    let result = 0;
+    return function (num) {
+        result += num;
+        console.log(result);
+    };
+};
+
+const sum = calcFunction();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));

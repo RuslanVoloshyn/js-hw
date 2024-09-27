1f4bdbe536364caff761d431009e57a349ad32dd
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-27 12:28:36 +0200

    Table

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const myTable = document.getElementById('myTable');
+
+let count = 1;
+
+for (let i = 0; i < 10; i++) {
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < 10; j++) {
+        const cell = document.createElement('td');
+        cell.textContent = count;
+
+        count++;
+
+        row.appendChild(cell);
+    }
+
+    myTable.appendChild(row);
+}

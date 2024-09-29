3c10d1ffd8675bdad2554d665d0c6c9bfb5e6b11
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-09-29 13:01:27 +0200

    random img

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const IMF_FOLDER = './../images/';
+const IMG_EXT = '.jpg';
+
+const appendHTNLElement = (parentEl = null, elementToAppend = null) => {
+    if (!parentEl || !elementToAppend) return;
+    parentEl.append(elementToAppend);
+};
+
+const getRandomNumbers = () => {
+    const num = Math.floor(Math.random() * 10);
+    if (num === 0) return 1;
+    return num;
+};
+
+const generateImages = (imagesName) => {
+    const img = document.createElement('img');
+    img.src = `${IMF_FOLDER}${imagesName + IMG_EXT}`;
+    img.alt = `${IMF_FOLDER}${imagesName + IMG_EXT}`;
+    img.title = `${IMF_FOLDER}${imagesName + IMG_EXT}`;
+    return img;
+};
+
+const imagesToAppend = generateImages(getRandomNumbers());
+
+appendHTNLElement(document.body, imagesToAppend);

d7aceb06cec774c17cede45b3adf14bad66d64f8
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-10-02 19:31:04 +0200

    update table

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,34 @@
 'use strict';
 
-const generateTable = () => {
+const createRow = (cellsInRow) => {
+    const row = document.createElement('tr');
+    for (let i = 0; i < cellsInRow.length; i++) {
+        row.append(cellsInRow[i]);
+    }
+    return row;
+};
+
+const createCell = (content) => {
+    const cell = document.createElement('td');
+    cell.innerHTML = content;
+    return cell;
+};
+
+const generateTable = (rows, cols) => {
     const myTable = document.getElementById('myTable');
     const tbody = document.createElement('tbody');
-
     myTable.append(tbody);
 
     let count = 1;
 
-    for (let i = 0; i < 10; i++) {
-        const row = document.createElement('tr');
-
-        for (let j = 0; j < 10; j++) {
-            const cell = document.createElement('td');
-            cell.innerHTML = count;
-
+    for (let i = 0; i < rows; i++) {
+        const cells = [];
+        for (let j = 0; j < cols; j++) {
+            cells.push(createCell(count));
             count++;
-
-            row.append(cell);
         }
-
-        tbody.append(row);
+        tbody.append(createRow(cells));
     }
 };
 
-generateTable();
+generateTable(10, 10);

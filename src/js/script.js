2f2b291c4fc6a65d59d287f817e3272762857b4e
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-10-03 19:11:42 +0200

    div occasion

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const names = document.getElementById('myName');
+const info = document.getElementById('focus');
+
+names.addEventListener('focus', () => {
+    info.style.display = 'block';
+});
+
+names.addEventListener('blur', () => {
+    info.style.display = 'none';
+});

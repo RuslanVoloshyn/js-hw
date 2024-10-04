b5583c4a038a9ad97bbfe27390172bd62bb5da1b
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-10-04 20:38:35 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,24 @@
 'use strict';
 
-const names = document.getElementById('myName');
-const info = document.getElementById('focus');
+const handleFocusBlur = (inputId, infoText) => {
+    const inputField = document.getElementById('myName');
+    const info = document.createElement('div');
 
-names.addEventListener('focus', () => {
-    info.style.display = 'block';
-});
+    info.id = 'focus-info';
 
-names.addEventListener('blur', () => {
     info.style.display = 'none';
-});
+
+    info.innerHTML = infoText;
+
+    document.body.appendChild(info);
+
+    inputField.addEventListener('focus', () => {
+        info.style.display = 'block';
+    });
+
+    inputField.addEventListener('blur', () => {
+        info.style.display = 'none';
+    });
+};
+
+handleFocusBlur('myName', 'This is some information');

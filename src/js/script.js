3e57f124ca91bd75cb71e15bd0d103f36e15dc5a
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-10-24 12:34:58 +0200

    user

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const user = {
+    _name: '',
+    _age: 0,
+
+    get name() {
+        return this._name;
+    },
+
+    set name(value) {
+        this._name = value;
+    },
+
+    get age() {
+        return this._age;
+    },
+
+    set age(value) {
+        if (typeof value === 'number' && value >= 0 && value <= 120) {
+            this._age = value;
+        } else {
+            console.log('Помилка: вік має бути числом від 0 до 120');
+        }
+    },
+};
+
+user.name = 'John';
+console.log(user.name);
+
+user.age = 25;
+console.log(user.age);
+
+user.age = 125;
+console.log(user.age);

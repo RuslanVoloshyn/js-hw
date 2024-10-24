4893667f812e0112aaae30f1c3d8428a9a6d4db3
Ruslan_Voloshyn <ruslanvoloshyn25@gmail.com>
2024-10-24 12:12:12 +0200

    deepFreeze

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+function deepFreeze(obj) {
+    Object.freeze(obj);
+
+    Object.keys(obj).forEach((key) => {
+        const value = obj[key];
+
+        if (
+            typeof value === 'object' &&
+            value !== null &&
+            !Object.isFrozen(value)
+        ) {
+            deepFreeze(value);
+        }
+    });
+
+    return obj;
+}
+
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            },
+        },
+    },
+};
+
+deepFreeze(user);
+
+user.data.a = 100;
+console.log(user.data.a);
